e70de05e1ebbcf4fc5df3f5e7fbec50aab18310c
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-05-15 18:50:14 +0200

    Questions improved

--- drinking/src/main/java/com/bespoke/drinking/DrinkingApplication.java
+++ drinking/src/main/java/com/bespoke/drinking/DrinkingApplication.java
@@ -26,79 +26,86 @@ public class DrinkingApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DrinkingApplication.class, args);
+		questions();
+		//bestDrinks();
+	}
+	
+	private static void questions() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieSession kSession = kContainer.newKieSession("questions");
 		
-//		KieServices ks = KieServices.Factory.get();
-//		KieContainer kContainer = ks.getKieClasspathContainer();
-//		KieSession kSession = kContainer.newKieSession("questions");
-//		
-//		User user = new User();
-//		user.setId(2);
-//		user.setCity("NS");
-//		user.setPassword("markuza2");
-//		user.setUsername("markuza2");
-//		
-//		Preference p = new Preference();
-//		p.setHot(null);
-//		user.setPreference(p);
-//		
-//		ArrayList<Question> questions = new ArrayList<>();
-//		
-//		Question q = new Question();
-//		q.setId(1);
-//		q.setText("Do you like hot or cold drinks?");
-//		
-//		ArrayList<Answer> answers = new ArrayList<>();
-//		
-//		Answer hot = new Answer();
-//		hot.setId(1);
-//		hot.setText("Hot");
-//		hot.setAnswerNumber(0);
-//		
-//		Answer cold = new Answer();
-//		cold.setId(2);
-//		cold.setText("Cold");
-//		cold.setAnswerNumber(1);
-//		
-//		answers.add(cold);
-//		answers.add(hot);
-//		
-//		q.setAnswers(answers);
-//		q.setSelectedAnswer(hot);
-//		
-//		questions.add(q);
-//		user.setAnsweredQuestions(questions);
-//		
-//		Question q2 = new Question();
-//		q2.setId(2);
-//		q2.setText("Do you drink coffee or tea more?");
-//		
-//		ArrayList<Answer> answers2 = new ArrayList<>();
-//		
-//		Answer coffee = new Answer();
-//		coffee.setId(3);
-//		coffee.setText("Coffee");
-//		coffee.setAnswerNumber(0);
-//		
-//		Answer tea = new Answer();
-//		tea.setId(4);
-//		tea.setText("Tea");
-//		tea.setAnswerNumber(1);
-//		
-//		answers2.add(coffee);
-//		answers2.add(tea);
-//		
-//		q2.setAnswers(answers2);
-//		q2.setSelectedAnswer(coffee);
-//		
-//		questions.add(q2);
-//		user.setAnsweredQuestions(questions);
-//		
-//		kSession.insert(user);
-//		int fired = kSession.fireAllRules();
-//		
-//		System.out.println("Number of Rules executed = " + fired);
-//		System.out.println("Preference (Is hot?): " + p.getHot());
+		User user = new User();
+		user.setId(2);
+		user.setCity("NS");
+		user.setPassword("markuza2");
+		user.setUsername("markuza2");
 		
+		Preference p = new Preference();
+		p.setHot(null);
+		user.setPreference(p);
+		
+		ArrayList<Question> questions = new ArrayList<>();
+		
+		Question q = new Question();
+		q.setId(1);
+		q.setText("Do you like hot or cold drinks?");
+		
+		ArrayList<Answer> answers = new ArrayList<>();
+		
+		Answer hot = new Answer();
+		hot.setId(1);
+		hot.setText("Hot");
+		hot.setAnswerNumber(0);
+		
+		Answer cold = new Answer();
+		cold.setId(2);
+		cold.setText("Cold");
+		cold.setAnswerNumber(1);
+		
+		answers.add(cold);
+		answers.add(hot);
+		
+		q.setAnswers(answers);
+		q.setSelectedAnswer(hot);
+		
+		questions.add(q);
+		user.setAnsweredQuestions(questions);
+		
+		Question q2 = new Question();
+		q2.setId(2);
+		q2.setText("Do you drink coffee or tea more?");
+		
+		ArrayList<Answer> answers2 = new ArrayList<>();
+		
+		Answer coffee = new Answer();
+		coffee.setId(3);
+		coffee.setText("Coffee");
+		coffee.setAnswerNumber(0);
+		
+		Answer tea = new Answer();
+		tea.setId(4);
+		tea.setText("Tea");
+		tea.setAnswerNumber(1);
+		
+		answers2.add(coffee);
+		answers2.add(tea);
+		
+		q2.setAnswers(answers2);
+		q2.setSelectedAnswer(coffee);
+		
+		questions.add(q2);
+		user.setAnsweredQuestions(questions);
+		
+		kSession.insert(user);
+		int fired = kSession.fireAllRules();
+		
+		System.out.println("Number of Rules executed = " + fired);
+		System.out.println("Preference (Is hot?): " + p.getHot());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void bestDrinks() {
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
 		KieSession rec = kContainer.newKieSession("questions");
@@ -203,6 +210,5 @@ public class DrinkingApplication {
 		List<Drink> bestDrinks = (List<Drink>) rec.getGlobal("bestDrinks");
 		System.out.println("Number of Rules executed = " + fired);
 		System.out.println(bestDrinks);
-		
 	}
 }
