8cfeb41aaf771b5b134bf266ee965054d2d8f24b
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-05-22 18:11:00 +0200

    Add drinks form css fixes

--- drinking-frontend/src/Components/AddDrinkModal/AddDrinkModal.tsx
+++ drinking-frontend/src/Components/AddDrinkModal/AddDrinkModal.tsx
@@ -1,14 +1,12 @@
 import {
-  Button,
-  Card,
-  CardBody,
-  Form,
-  FormFeedback,
-  FormGroup,
-  Input,
-  Label,
-  ListGroup,
-  ListGroupItem,
+	Button,
+	Form,
+	FormFeedback,
+	FormGroup,
+	Input,
+	Label,
+	ListGroup,
+	ListGroupItem,
 } from "reactstrap";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { addDrinkSchema } from "./AddDrinkSchema";
@@ -25,147 +23,137 @@ import DrinkIngredients from "../DrinkIngredients/DrinkIngredients";
 
 // toast.configure();
 const AddDrinkModal = (props: { restaurant: Restaurant; close: Function }) => {
-  const customId = "addDrinkModal";
-  const [hot, setHot] = useState(false);
-  const [alcoholic, setAlcoholic] = useState(false);
-  const [caffeine, setCaffeine] = useState(false);
-  const [ingredients, setIngredients] = useState<string[]>([]);
-  const [taste, setTaste] = useState<any>({
-    SWEET: 0,
-    SOUR: 0,
-    BITTER: 0,
-    UMAMI: 0,
-    SALTY: 0,
-  });
+	const customId = "addDrinkModal";
+	const [hot, setHot] = useState(false);
+	const [alcoholic, setAlcoholic] = useState(false);
+	const [caffeine, setCaffeine] = useState(false);
+	const [ingredients, setIngredients] = useState<string[]>([]);
+	const [taste, setTaste] = useState<any>({
+		SWEET: 0,
+		SOUR: 0,
+		BITTER: 0,
+		UMAMI: 0,
+		SALTY: 0,
+	});
 
-  const closeModal = () => {
-    props.close && props.close();
-  };
+	const closeModal = () => {
+		props.close && props.close();
+	};
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(addDrinkSchema),
-    mode: "onChange",
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(addDrinkSchema),
+		mode: "onChange",
+	});
 
-  const addDrink = (drink: Drink) => {
-    drink.hot = hot;
-    drink.alcoholic = alcoholic;
-    drink.caffeine = caffeine;
-    drink.taste = taste;
-    drink.ingredients = ingredients;
+	const addDrink = (drink: Drink) => {
+		drink.hot = hot;
+		drink.alcoholic = alcoholic;
+		drink.caffeine = caffeine;
+		drink.taste = taste;
+		drink.ingredients = ingredients;
 
-    axios
-      .put(ADD_DRINK_TO_RESTAURANT + props.restaurant.id, drink)
-      .then((res: any) => {
-        closeModal();
-      })
-      .catch((err: any) => {});
-  };
+		axios
+			.put(ADD_DRINK_TO_RESTAURANT + props.restaurant.id, drink)
+			.then((res: any) => {
+				closeModal();
+			})
+			.catch((err: any) => {});
+	};
 
-  const changeTaste = (flavour: string, tasteValue: number) => {
-    let copy = taste;
-    copy[flavour] = tasteValue;
-    setTaste(copy);
-  };
+	const changeTaste = (flavour: string, tasteValue: number) => {
+		let copy = taste;
+		copy[flavour] = tasteValue;
+		setTaste(copy);
+	};
 
-  const addIngredient = (ingredient: string) => {
-    const copy = [...ingredients];
-    if (copy.find((i) => i === ingredient) === undefined) {
-      copy.push(ingredient);
-      setIngredients(copy);
-    }
-  };
+	const addIngredient = (ingredient: string) => {
+		const copy = [...ingredients];
+		if (copy.find((i) => i === ingredient) === undefined) {
+			copy.push(ingredient);
+			setIngredients(copy);
+		}
+	};
 
-  return (
-    <div>
-      <Card
-        className="card-add-drink"
-        style={{ backgroundColor: "#DEEDE6", borderColor: "black" }}
-      >
-        <CardBody>
-          <div className="drink-basic">
-            <Form>
-              <FormGroup>
-                <Label>Name</Label>
-                <Input
-                  type="text"
-                  name="name"
-                  placeholder="name"
-                  invalid={errors.name?.message}
-                  innerRef={register}
-                />
-                <FormFeedback>{errors.name?.message}</FormFeedback>
-              </FormGroup>
+	return (
+		<>
+			<Form>
+				<FormGroup>
+					<Label>Name</Label>
+					<Input
+						type="text"
+						name="name"
+						placeholder="name"
+						invalid={errors.name?.message}
+						innerRef={register}
+					/>
+					<FormFeedback>{errors.name?.message}</FormFeedback>
+				</FormGroup>
 
-              <FormGroup>
-                <Label>Hot</Label>
-                <Input
-                  type="checkbox"
-                  name="hot"
-                  innerRef={register}
-                  onChange={() => setHot(!hot)}
-                />
-              </FormGroup>
+				<FormGroup>
+					<Label>Hot</Label>
+					<Input
+						className="ml-2"
+						type="checkbox"
+						name="hot"
+						innerRef={register}
+						onChange={() => setHot(!hot)}
+					/>
+				</FormGroup>
 
-              <FormGroup>
-                <Label>Alcoholic</Label>
-                <Input
-                  type="checkbox"
-                  name="alcoholic"
-                  innerRef={register}
-                  onChange={() => setAlcoholic(!alcoholic)}
-                />
-              </FormGroup>
+				<FormGroup>
+					<Label>Alcoholic</Label>
+					<Input
+						className="ml-2"
+						type="checkbox"
+						name="alcoholic"
+						innerRef={register}
+						onChange={() => setAlcoholic(!alcoholic)}
+					/>
+				</FormGroup>
 
-              <FormGroup>
-                <Label>Caffeine</Label>
-                <Input
-                  type="checkbox"
-                  name="caffeine"
-                  innerRef={register}
-                  onChange={() => setCaffeine(!caffeine)}
-                />
-              </FormGroup>
+				<FormGroup>
+					<Label>Caffeine</Label>
+					<Input
+						className="ml-2"
+						type="checkbox"
+						name="caffeine"
+						innerRef={register}
+						onChange={() => setCaffeine(!caffeine)}
+					/>
+				</FormGroup>
 
-              <FormGroup>
-                <Label>Texture</Label>
-                <Input type="select" name="texture" innerRef={register}>
-                  <option>LIQUID</option>
-                  <option>HALF_THICC</option>
-                  <option>THICC</option>
-                </Input>
-              </FormGroup>
-            </Form>
+				<FormGroup>
+					<Label>Texture</Label>
+					<Input type="select" name="texture" innerRef={register}>
+						<option>LIQUID</option>
+						<option>HALF_THICC</option>
+						<option>THICC</option>
+					</Input>
+				</FormGroup>
+			</Form>
 
-            <div className="drink-tastes">
-              <DrinkTaste changeTastes={changeTaste} />
-            </div>
-          </div>
+			<DrinkTaste changeTastes={changeTaste} />
 
-          <div className="div-ingredients">
-            <DrinkIngredients addIngredient={addIngredient} />
-            <ListGroup>
-              {ingredients.map((item, index) => (
-                <ListGroupItem key={index}>{item}</ListGroupItem>
-              ))}
-            </ListGroup>
-          </div>
+			<DrinkIngredients addIngredient={addIngredient} />
+			<ListGroup style={{ paddingTop: "5px" }}>
+				{ingredients.map((item, index) => (
+					<ListGroupItem key={index}>{item}</ListGroupItem>
+				))}
+			</ListGroup>
 
-          <Button
-            className="registruj-login-btn"
-            color="primary"
-            type="button"
-            onClick={handleSubmit(addDrink)}
-          >
-            Add Drink
-          </Button>
-        </CardBody>
-      </Card>
-    </div>
-  );
+			<Button
+				className="registruj-login-btn"
+				color="primary"
+				type="button"
+				onClick={handleSubmit(addDrink)}
+			>
+				Add Drink
+			</Button>
+		</>
+	);
 };
 export default AddDrinkModal;
--- drinking-frontend/src/Containers/Restaurants/Restaurants.tsx
+++ drinking-frontend/src/Containers/Restaurants/Restaurants.tsx
@@ -1,16 +1,17 @@
 import {
-  Button,
-  Card,
-  CardBody,
-  CardTitle,
-  Form,
-  FormFeedback,
-  FormGroup,
-  Input,
-  Label,
-  Modal,
-  ModalHeader,
-  Table,
+	Button,
+	Card,
+	CardBody,
+	CardTitle,
+	Form,
+	FormFeedback,
+	FormGroup,
+	Input,
+	Label,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Table,
 } from "reactstrap";
 import UnauthenticatedNavbar from "../../Navbars/UnauthenticatedNavbar";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -18,12 +19,7 @@ import { restaurantSchema } from "./RestaurantSchema";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import { User } from "../../Model/User";
-import {
-  GET_ALL_RESTAURANTS,
-  POST_RESTAURANT,
-  POST_USER,
-} from "../../api-routes";
+import { GET_ALL_RESTAURANTS, POST_RESTAURANT } from "../../api-routes";
 import { useForm } from "react-hook-form";
 import { Restaurant } from "../../Model/Restaurant";
 import { useEffect, useState } from "react";
@@ -31,147 +27,149 @@ import AddDrinkModal from "../../Components/AddDrinkModal/AddDrinkModal";
 
 // toast.configure();
 const Restaurants = () => {
-  const customId = "restaurants";
-  const [restaurants, setRestaurants] = useState<Array<Restaurant> | null>(
-    null
-  );
-  const [showModal, setShowModal] = useState(false);
-  const [currentRestaurant, setRestaurant] = useState<Restaurant | null>(null);
+	const customId = "restaurants";
+	const [restaurants, setRestaurants] = useState<Array<Restaurant> | null>(
+		null
+	);
+	const [showModal, setShowModal] = useState(false);
+	const [currentRestaurant, setRestaurant] = useState<Restaurant | null>(null);
 
-  const toggle = () => setShowModal(!showModal);
+	const toggle = () => setShowModal(!showModal);
 
-  useEffect(() => {
-    getRestaurants();
-  }, []);
+	useEffect(() => {
+		getRestaurants();
+	}, []);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(restaurantSchema),
-    mode: "onChange",
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(restaurantSchema),
+		mode: "onChange",
+	});
 
-  const getRestaurants = () => {
-    axios
-      .get(GET_ALL_RESTAURANTS)
-      .then((res: any) => {
-        setRestaurants(res.data);
-        console.log(res.data);
-      })
-      .catch((err: any) => {});
-  };
+	const getRestaurants = () => {
+		axios
+			.get(GET_ALL_RESTAURANTS)
+			.then((res: any) => {
+				setRestaurants(res.data);
+				console.log(res.data);
+			})
+			.catch((err: any) => {});
+	};
 
-  const addRestaurant = (restaurant: Restaurant) => {
-    axios
-      .post(POST_RESTAURANT, restaurant)
-      .then((res: any) => {
-        getRestaurants();
-      })
-      .catch((err: any) => {});
-  };
+	const addRestaurant = (restaurant: Restaurant) => {
+		axios
+			.post(POST_RESTAURANT, restaurant)
+			.then((res: any) => {
+				getRestaurants();
+			})
+			.catch((err: any) => {});
+	};
 
-  const addDrink = (restaurant: Restaurant) => {
-    toggle();
-    setRestaurant(restaurant);
-  };
+	const addDrink = (restaurant: Restaurant) => {
+		toggle();
+		setRestaurant(restaurant);
+	};
 
-  return (
-    <div>
-      <UnauthenticatedNavbar />
+	return (
+		<div>
+			<UnauthenticatedNavbar />
 
-      <Card
-        className="card-login-registracija"
-        style={{ backgroundColor: "#DEEDE6", borderColor: "black" }}
-      >
-        <CardBody>
-          <CardTitle tag="h2">Add Restaurant</CardTitle>
-          <Form className="form-login-registracija">
-            <FormGroup>
-              <Label>Name</Label>
-              <Input
-                type="text"
-                name="name"
-                placeholder="name"
-                invalid={errors.name?.message}
-                innerRef={register}
-              />
-              <FormFeedback>{errors.name?.message}</FormFeedback>
-            </FormGroup>
+			<Card
+				className="card-login-registracija"
+				style={{ backgroundColor: "#DEEDE6", borderColor: "black" }}
+			>
+				<CardBody>
+					<CardTitle tag="h2">Add Restaurant</CardTitle>
+					<Form className="form-login-registracija">
+						<FormGroup>
+							<Label>Name</Label>
+							<Input
+								type="text"
+								name="name"
+								placeholder="name"
+								invalid={errors.name?.message}
+								innerRef={register}
+							/>
+							<FormFeedback>{errors.name?.message}</FormFeedback>
+						</FormGroup>
 
-            <FormGroup>
-              <Label>City</Label>
-              <Input
-                type="text"
-                name="city"
-                placeholder="NS"
-                invalid={errors.city?.message}
-                innerRef={register}
-              />
-              <FormFeedback>{errors.city?.message}</FormFeedback>
-            </FormGroup>
+						<FormGroup>
+							<Label>City</Label>
+							<Input
+								type="text"
+								name="city"
+								placeholder="NS"
+								invalid={errors.city?.message}
+								innerRef={register}
+							/>
+							<FormFeedback>{errors.city?.message}</FormFeedback>
+						</FormGroup>
 
-            <Button
-              className="registruj-login-btn"
-              color="primary"
-              type="button"
-              onClick={handleSubmit(addRestaurant)}
-            >
-              Add restaurant
-            </Button>
-          </Form>
-        </CardBody>
-      </Card>
+						<Button
+							className="registruj-login-btn"
+							color="primary"
+							type="button"
+							onClick={handleSubmit(addRestaurant)}
+						>
+							Add restaurant
+						</Button>
+					</Form>
+				</CardBody>
+			</Card>
 
-      <Card
-        className="card-login-registracija"
-        style={{ backgroundColor: "#DEEDE6", borderColor: "black" }}
-      >
-        <CardBody>
-          <Table bordered className="div-dokumenti">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>City</th>
-                <th>Add drinks</th>
-              </tr>
-            </thead>
+			<Card
+				className="card-login-registracija"
+				style={{ backgroundColor: "#DEEDE6", borderColor: "black" }}
+			>
+				<CardBody>
+					<Table bordered className="div-dokumenti">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>City</th>
+								<th>Add drinks</th>
+							</tr>
+						</thead>
 
-            <tbody>
-              {restaurants?.map((restaurant) => {
-                return (
-                  <tr key={restaurant.id}>
-                    <td>{restaurant.name}</td>
-                    <td>{restaurant.city}</td>
-                    <td>
-                      <Button
-                        className="table-button"
-                        color="primary"
-                        type="button"
-                        onClick={() => addDrink(restaurant)}
-                      >
-                        Add Drink
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </CardBody>
-      </Card>
+						<tbody>
+							{restaurants?.map((restaurant) => {
+								return (
+									<tr key={restaurant.id}>
+										<td>{restaurant.name}</td>
+										<td>{restaurant.city}</td>
+										<td>
+											<Button
+												className="table-button"
+												color="primary"
+												type="button"
+												onClick={() => addDrink(restaurant)}
+											>
+												Add Drink
+											</Button>
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</Table>
+				</CardBody>
+			</Card>
 
-      <Modal isOpen={showModal} toggle={toggle} className="add-drink-modal">
-        <ModalHeader toggle={toggle}>Add Drink</ModalHeader>
+			<Modal isOpen={showModal} toggle={toggle} className="add-drink-modal">
+				<ModalHeader toggle={toggle} style={{ backgroundColor: "#DEEDE6" }}>
+					Add Drink
+				</ModalHeader>
 
-        {currentRestaurant && (
-          <div>
-            <AddDrinkModal restaurant={currentRestaurant} close={toggle} />
-          </div>
-        )}
-      </Modal>
-    </div>
-  );
+				{currentRestaurant && (
+					<ModalBody style={{ backgroundColor: "#DEEDE6" }}>
+						<AddDrinkModal restaurant={currentRestaurant} close={toggle} />
+					</ModalBody>
+				)}
+			</Modal>
+		</div>
+	);
 };
 export default Restaurants;
